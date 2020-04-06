216b290643cd6948a2345fd18dcad1882162e2c1
Jani Maljanen <jani.maljanen@vaimo.com>
2020-04-06 22:30:08 +0300

    update

--- src/Dynamsoft.js
+++ src/Dynamsoft.js
@@ -1,6 +1,6 @@
 import Dynamsoft from "dynamsoft-javascript-barcode";
-Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@7.3.0-v0/dist/";
+Dynamsoft.BarcodeReader.engineResourcePath = "https://cdn.jsdelivr.net/npm/dynamsoft-javascript-barcode@7.3.0-v4/dist/";
 // Please visit https://www.dynamsoft.com/CustomerPortal/Portal/TrialLicense.aspx to get a trial license
-Dynamsoft.BarcodeReader.productKeys = "t0068NQAAAJF5p9U9SMPLVeBTd+ecDU4Ze9WJ+q6999Dpg0z3kIYdix9d7o5y4g6cb2Ot/7/5uTcne9nVBiEWNoi74sCBgXM=";
+Dynamsoft.BarcodeReader.productKeys = "t0068NQAAACIUn2QPVKueIbvL/QS5F06q6PtYrRYtqcx0YvjfBiQDI132l33VGNoJv5BtGcyChU6gbD0jw9TDJFU81Q+9POc=";
 // Dynamsoft.BarcodeReader._bUseFullFeature = true; // Control of loading min wasm or full wasm.
 export default Dynamsoft;
